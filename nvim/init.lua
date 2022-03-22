10bf78586bf4460cfc011284d9fd020ca0bf714e
Andreas Alin <andreas.alin@gmail.com>
2022-03-22 10:56:03 -0500

    Configure toggleterm

--- nvim/init.lua
+++ nvim/init.lua
@@ -653,3 +653,25 @@ wk.register({
   D = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type definition" },
   K = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Show type" }
 }, { prefix = "<leader>" })
+
+require("toggleterm").setup{
+  size = 20,
+  open_mapping = [[<c-t>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = 'float',
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = 'curved',
+    winblend = 10,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    }
+  }
+}
