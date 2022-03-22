88ee09358dc0b57dba1081e9e9b77aa4da771a55
Andreas Alin <andreas.alin@gmail.com>
2022-03-22 10:55:34 -0500

    Fix packer install

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,14 @@
 --
 -- Based on https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 
-vim.cmd [[
-  packadd packer.nvim
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
   augroup Packer
     autocmd!
     autocmd BufWritePost init.lua PackerCompile
